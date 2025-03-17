88446de7b58b210bfd093cefc09c7ed68380417b
MendelBrio <mrudski@gobrio.com>
2025-03-17 17:33:57 +0000

    Renamed to Custom_MDAPI, Updated yml commands to sf

--- unpackaged/config/litify-mdapi/scripts/update_consumer_key.sh
+++ unpackaged/config/litify-mdapi/scripts/update_consumer_key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Extract the Consumer Key and store it in an environment variable
-export SECURE_ACCESS_KEY=$(grep -oP '<consumerKey>\K[^<]+' unpackaged/config/litify/mdapi/src/connectedApps/LitifyMDAPI.connectedApp)
+export SECURE_ACCESS_KEY=$(grep -oP '<consumerKey>\K[^<]+' unpackaged/config/litify/mdapi/src/connectedApps/Custom_Litify_MDAPI.connectedApp)
 
 # (Optional) Print the value to verify
 echo $SECURE_ACCESS_KEY
--- unpackaged/config/litify-mdapi/scripts/update_domains.sh
+++ unpackaged/config/litify-mdapi/scripts/update_domains.sh
@@ -5,7 +5,7 @@ echo "Script started"
 consumerkey_value=$(cat config/consumerkey.txt)
 echo "Domain value read: $consumerkey_value"
 
-sed -i "s|CONSUMER_KEY|$consumerkey_value|g" ../src/main/default/authproviders/LitifyMDAPI.authprovider-meta.xml
+sed -i "s|CONSUMER_KEY|$consumerkey_value|g" ../src/main/default/authproviders/Custom_Litify_MDAPI.authprovider-meta.xml
 echo "Replaced CONSUMER_KEY in authprovider-meta.xml"
 
 echo "Script completed"
